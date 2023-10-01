b859652b514fa9865bbab7c53d47ed4e41955009
User <user@Crrips-MacBook-Pro.local>
2023-10-01 17:41:07 +0400

    Created git ignore

--- CPP0/ex01/PhoneBook.hpp
+++ CPP0/ex01/PhoneBook.hpp
@@ -20,19 +20,46 @@ class PhoneBook
 	string phoneNumber;
 	string darkSecret;
 
+	void	onlyLetters(string *str)
+	{
+		for (size_t i = 0; i < str->length(); i++)
+		{
+			if (!isalpha((*str)[i]))
+			{
+				cout << "error: only letters" << endl;
+				exit(1);
+			}
+		}
+	}
+
+	void	onlyDigits(string *str)
+	{
+		for (size_t i = 0; i < str->length(); i++)
+		{
+			if (!isdigit((*str)[i]))
+			{
+				cout << "error: only digits" << endl;
+				exit(1);
+			}
+		}
+	}
+
 	void	addNew()
 	{
 		cout << "Enter first name: ";
 		cin >> this->firstName;
+		onlyLetters(&this->firstName);
 		inputCheck(&this->firstName);
 		cout << "Enter last name: ";
 		cin >> this->lastName;
+		onlyLetters(&this->lastName);
 		inputCheck(&this->lastName);
 		cout << "Enter nickname: ";
 		cin >> this->nickName;
 		inputCheck(&this->nickName);
 		cout << "Enter phone number: ";
 		cin >> this->phoneNumber;
+		onlyDigits(&this->phoneNumber);
 		inputCheck(&this->phoneNumber);
 		cout << "Enter dark secret: ";
 		cin >> this->darkSecret;
