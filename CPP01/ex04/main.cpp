ff1d2414e902c038c9ef28a576d3bfad2af718be
User <user@Crrips-MacBook-Pro.local>
2023-12-03 18:09:30 +0400

    01 in progress

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <fstream>
 
+void my_string_iterator_replacer(std::string &str, std::string const &s1, std::string const &s2)
+{
+	size_t pos = 0;
+	size_t len = s1.length();
+
+	while ((pos = str.find(s1, pos)) != std::string::npos)
+	{
+		str.erase(pos, len);
+		str.insert(pos, s2);
+		pos += s2.length();
+	}
+}
+
 int main(int argc, char **argv)
 {
 	std::ifstream	ifs;
 	std::ofstream	ofs;
-	std::string		filename;
+	std::string		fileName;
 	std::string		s1;
 	std::string		s2;
 
@@ -14,7 +27,7 @@ int main(int argc, char **argv)
 		std::cout << "Usage: ./replacer <filename> <string to replace> <string to replace with>" << std::endl;
 		return 1;
 	}
-	filename = argv[1];
+	fileName = argv[1];
 	s1 = argv[2];
 	s2 = argv[3];
 	if (s1.empty() || s2.empty())
@@ -22,12 +35,18 @@ int main(int argc, char **argv)
 		std::cout << "Error: empty string" << std::endl;
 		return 1;
 	}
-	ifs.open(filename);
+	ifs.open(fileName);
 	if (!ifs.is_open())
 	{
 		std::cout << "Error: could not open file" << std::endl;
 		return 1;
 	}
-	
+	std::string fileContent((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+	ifs.close();
+	my_string_iterator_replacer(fileContent, s1, s2);
+	std::cout << fileContent;
+	ofs.open(fileName + ".replace");
+	ofs << fileContent;
+	ofs.close();
     return 0;
 }
