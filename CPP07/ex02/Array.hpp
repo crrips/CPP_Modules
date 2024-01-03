00a842e7014a6125925acf840de8cd8f9e361efb
Ahadzhan Piloian <ahadzhan.piloian@gmail.com>
2024-01-03 19:44:16 +0400

    007 in progress

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <iostream>
+
 template <typename T>
 class Array
 {
+    private:
+        T* _array;
+    public:
+        Array(void) : _array(NULL) {};
+        Array(unsigned int n) : _array(new T[n]) {};
+        Array(const Array & other) : _array(NULL) { *this = other; };
+        ~Array(void) { delete [] _array; };
 
+        
 };
